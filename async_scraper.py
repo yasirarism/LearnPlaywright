98f330cc493d5b9117fcb44429a5ba77f8a7f2a7
Yasir Aris M <git@yasirdev.my.id>
2024-09-03 13:18:35 +0700

    Update async_scraper.py

--- async_scraper.py
+++ async_scraper.py
@@ -15,7 +15,7 @@ async def scrape(url):
             # Wait for the element to be present and extract text
             # await page.wait_for_selector('h1')
             # title = await page.locator('h1').text_content()
-            print(page.content())
+            print(await page.content())
 
         except Exception as e:
             print(f"Error scraping {url}: {e}")
