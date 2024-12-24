7a0dc6fba45ca3b975afbb2b922b3d2ac78f865c
Yasir Aris M <git@yasirdev.my.id>
2024-12-24 22:18:35 +0700

    Update async_scraper.py

--- async_scraper.py
+++ async_scraper.py
@@ -56,7 +56,7 @@ def get_video_url():
         })
 
         driver.get(dood_url)
-        LOGGER.info(driver.get_text())
+        LOGGER.info(driver.page_source)
         time.sleep(5)
    
         video_url = None
