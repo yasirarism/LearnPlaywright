091d9231f70e1b6ff932f077c1e03db418e567d8
Yasir Aris M <git@yasirdev.my.id>
2024-12-24 22:12:28 +0700

    Update async_scraper.py

--- async_scraper.py
+++ async_scraper.py
@@ -60,10 +60,6 @@ def get_video_url():
    
         video_url = None
         try:
-            all_elements = driver.find_elements(By.XPATH, "//*")
-            for element in all_elements:
-                src = element.get_attribute('src')
-                LOGGER.info(src)
             video_element = driver.find_element(By.XPATH, "//video")
             video_url = video_element.get_attribute("src")
         except Exception as e:
