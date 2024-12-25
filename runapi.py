0e75c65eeb4ae6eb72c577fb84ed666e5f7c4092
Yasir Aris M <git@yasirdev.my.id>
2024-12-25 20:06:13 +0700

    Update runapi.py

--- runapi.py
+++ runapi.py
@@ -4,4 +4,4 @@ import os
 port = os.environ.get("PORT", 80)
 
 if __name__ == "__main__":
-    uvicorn.run("async_scraper:app", host="0.0.0.0", port=int(port), reload=False)
+    uvicorn.run("async_scraper:flask_app", host="0.0.0.0", port=int(port), reload=False)
