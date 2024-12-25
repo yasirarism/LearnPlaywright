52b73ddb01051d1e92b9f83ed9786c50a7ebce76
Yasir Aris M <git@yasirdev.my.id>
2024-12-25 08:26:50 +0700

    Update async_scraper.py

--- async_scraper.py
+++ async_scraper.py
@@ -42,7 +42,7 @@ def get_video_url():
     domain = urlparse(dood_url).netloc
 
     options = Options()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument(f"user-agent=Mozilla/5.0 (Linux; Android 15; Pixel 9 Pro Fold Build/AP3A.241005.015; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/129.0.6668.100 Mobile Safari/537.36")
