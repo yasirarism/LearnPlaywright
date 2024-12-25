f92f84559639e4925e634d9d5cb27ab97314da94
Yasir Aris M <git@yasirdev.my.id>
2024-12-25 22:45:22 +0700

    Update async_scraper.py

--- async_scraper.py
+++ async_scraper.py
@@ -46,6 +46,7 @@ def get_video_url():
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--proxy-server=http://103.148.130.107:8080")
     options.add_argument(f"user-agent=Mozilla/5.0 (Linux; Android 15; Pixel 9 Pro Fold Build/AP3A.241005.015; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/129.0.6668.100 Mobile Safari/537.36")
 
     # Use Chromium instead of Chrome
