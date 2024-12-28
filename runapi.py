753fe191cc3ea838857f4155d737326cc28e529a
Yasir Aris M <git@yasirdev.my.id>
2024-12-28 08:52:40 +0700

    Update runapi.py

--- runapi.py
+++ runapi.py
@@ -1,7 +1,7 @@
 import uvicorn
 import os
 
-port = os.environ.get("PORT", 80)
+port = os.environ.get("PORT", 8081)
 
 if __name__ == "__main__":
     uvicorn.run("async_scraper:dood_app", host="0.0.0.0", port=int(port), reload=False)
